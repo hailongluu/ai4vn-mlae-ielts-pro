81b7356115aa0bd50e4b6feae5bc0ae247fd0b94
long luu <hailongluu@gmail.com>
2019-08-15 01:21:57 +0700

    merge-adapter-errror /n

--- api_server.py
+++ api_server.py
@@ -36,10 +36,10 @@ def get_full_exam():
     return response
 
 
-@app.route("/exam/get_topic")
-def get_topic():
-    topics = data_collector.get_topic(20)
-    return jsonify(topics)
+# @app.route("/exam/get_topic")
+# def get_topic():
+#     topics = data_collector.get_topic(20)
+#     return jsonify(topics)
 
 
 @app.route("/exam/get_writing")
